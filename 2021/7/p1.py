d4073047bf9839d87a273e36c6d3a434477ffb5e
James Won <me@jwon.me>
2021-12-07 10:07:30 -0800

    day7 2021

---
+++ 2021/7/p1.py
@@ -0,0 +1,12 @@
+with open('input.txt') as f:
+    crab_positions = [int (i) for line in f for i in line.strip().split(',')]
+
+least_fuel_cost = None
+for pos in range(len(crab_positions)):
+    current_fuel_cost = 0
+    for crab in crab_positions:
+        current_fuel_cost += abs(crab - pos) 
+    if least_fuel_cost is None or current_fuel_cost < least_fuel_cost:
+        least_fuel_cost = current_fuel_cost
+
+print(least_fuel_cost)
