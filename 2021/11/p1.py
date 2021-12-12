a2255fa8b88cd22f1cc8ed62abd5092b5ed44306
James Won <me@jwon.me>
2021-12-11 22:03:14 -0800

    day11 2021

---
+++ 2021/11/p1.py
@@ -0,0 +1,53 @@
+with open('input.txt') as f:
+    octopuses = [[int(char) for char in line.strip()] for line in f]
+    x_max = len(octopuses[0])
+    y_max = len(octopuses)
+    x_min = 0
+    y_min = 0
+
+def run_step(octopuses):
+    octopuses = [[level+1 for level in row] for row in octopuses]
+    flashed = [[0 for _ in row] for row in octopuses]
+
+    while any(level > 9 and flashed[y_pos][x_pos] == 0 for y_pos, row in enumerate(octopuses) for x_pos, level in enumerate(row)):
+        for y_pos, row in enumerate(octopuses):
+            for x_pos, level in enumerate(row):
+                if level > 9 and flashed[y_pos][x_pos] == 0:
+                    flashed[y_pos][x_pos] = 1
+                    is_safe_left = x_pos-1 >= x_min
+                    is_safe_right = x_pos+1 < x_max
+                    is_safe_up = y_pos-1 >= y_min
+                    is_safe_down = y_pos+1 < y_max
+                    if is_safe_left:
+                        octopuses[y_pos][x_pos-1] += 1
+                    if is_safe_right:
+                        octopuses[y_pos][x_pos+1] += 1
+                    if is_safe_up:
+                        octopuses[y_pos-1][x_pos] += 1
+                    if is_safe_down:
+                        octopuses[y_pos+1][x_pos] += 1
+                    if is_safe_left and is_safe_up:
+                        octopuses[y_pos-1][x_pos-1] += 1
+                    if is_safe_left and is_safe_down:
+                        octopuses[y_pos+1][x_pos-1] += 1
+                    if is_safe_right and is_safe_up:
+                        octopuses[y_pos-1][x_pos+1] += 1
+                    if is_safe_right and is_safe_down:
+                        octopuses[y_pos+1][x_pos+1] += 1
+    
+    for y_pos, row in enumerate(octopuses):
+        for x_pos, level in enumerate(row):
+            if level > 9:
+                octopuses[y_pos][x_pos] = 0
+
+    return octopuses, sum(value for row in flashed for value in row)
+    
+total_flashes = 0
+for i in range(100):
+    octopuses, flashes = run_step(octopuses)
+    total_flashes += flashes
+    print(f'After Step #{i+1}')
+    print(*octopuses, sep='\n')
+
+print(total_flashes)
+
