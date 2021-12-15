5ab298cea28f0029743e52e4edaaca7d9a0bfb1d
James Won <me@jwon.me>
2021-12-15 13:51:52 -0800

    day15 2021

---
+++ 2021/15/p1.py
@@ -0,0 +1,49 @@
+from queue import PriorityQueue
+
+
+with open('input.txt') as f:
+    cavern = [[int(i) for i in line.strip()] for line in f]
+
+max_row = len(cavern)
+max_col = len(cavern[0])
+min_row = 0
+min_col = 0
+
+# for row in cavern:
+#     print(*row)
+
+def dijkstra(grid, start_row, start_col):
+    costs = [[float('inf') for i in row] for row in grid]
+    costs[start_row][start_col] = 0
+
+    q = PriorityQueue()
+    q.put((0, (start_row, start_col)))
+
+    visited = set()
+    while not q.empty():
+        _, (curr_row, curr_col) = q.get()
+        visited.add((curr_row, curr_col))
+        adjacent = []
+        if curr_col-1 >= min_col:  # left
+            adjacent.append((curr_row, curr_col-1))
+        if curr_col+1 < max_col:  # right
+            adjacent.append((curr_row, curr_col+1))
+        if curr_row-1 >= min_row:  # up
+            adjacent.append((curr_row-1, curr_col))
+        if curr_row+1 < max_row:  # down
+            adjacent.append((curr_row+1, curr_col))
+        for next_row, next_col in adjacent:
+            if (next_row, next_col) not in visited:
+                old_cost = costs[next_row][next_col]
+                new_cost = costs[curr_row][curr_col] + grid[next_row][next_col]
+                if new_cost < old_cost:
+                    q.put((new_cost, (next_row, next_col)))
+                    costs[next_row][next_col] = new_cost
+
+    return costs
+
+distances = dijkstra(cavern, 0, 0)
+for row in distances:
+    print(*row)
+
+print(distances[max_row-1][max_col-1])
